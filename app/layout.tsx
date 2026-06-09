392b8373b6b0a0192e0efabb39e02cc89c367389
admcrater <admcrater@gmail.com>
2026-06-09 17:49:30 -0300

    feat: add Google Tag Manager GTM-NTGDJBD

--- app/layout.tsx
+++ app/layout.tsx
@@ -122,6 +122,13 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="theme-color" content="#ffffff" />
+        <Script id="gtm" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NTGDJBD');`}
+        </Script>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=AW-673769621"
           strategy="afterInteractive"
@@ -159,6 +166,10 @@ export default function RootLayout({
       <body
         className={`${poppins.variable} antialiased`}
       >
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NTGDJBD"
+            height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe>
+        </noscript>
         <a
           href="#main-content"
           className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-[100] focus:bg-primary focus:text-white focus:px-4 focus:py-2 focus:rounded-md focus:text-sm focus:font-semibold"
