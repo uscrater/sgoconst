a100ab9ebf5fcbb5f0c0d86f4a205337a83f57fd
admcrater <admcrater@gmail.com>
2026-06-09 17:46:56 -0300

    feat: add Meta Pixel 810892378511223

--- app/layout.tsx
+++ app/layout.tsx
@@ -134,6 +134,27 @@ export default function RootLayout({
             gtag('config', 'AW-673769621');
           `}
         </Script>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '810892378511223');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img height="1" width="1" style={{display:'none'}}
+            src="https://www.facebook.com/tr?id=810892378511223&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </head>
       <body
         className={`${poppins.variable} antialiased`}
